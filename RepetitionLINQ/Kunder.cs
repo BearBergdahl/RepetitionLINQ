131955441c97e952d3fbef4460d70cad2c656157
Björn Bergdahl <84767875+BearBergdahl@users.noreply.github.com>
2023-11-23 10:45:32 +0100

    Add project files.

---
+++ RepetitionLINQ/Kunder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepetitionLINQ
+{
+    internal class Kund
+    {
+        public Kund(string name, string description) 
+        { 
+            this.Name = name;
+            this.Description = description;
+            this.Id = Guid.NewGuid();
+        }
+        public Guid Id { get; }
+        public string Name { get; }
+        public string Description { get; }
+    }
+}
---
+++ RepetitionLINQ/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+
+namespace RepetitionLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Kund> kunderTillForetaget = new List<Kund>();
+            kunderTillForetaget.Add(new Kund("ljugsrg", "hgsdrg"));
+            kunderTillForetaget.Add(new Kund("Alligator", "Krokodil"));
+            kunderTillForetaget.Add(new Kund("Alligator", "Kajman"));
+            kunderTillForetaget.Add(new Kund("Alligator", "Sköldpadda"));
+            var Something = kunderTillForetaget
+                .Where(kund => kund.Name == "Alligator")
+                .Select(kund => kund.Description)
+                .Where(description => description == "Sköldpadda").ToList();
+
+            List<Kund> resultOfWhere = new List<Kund>();   
+
+            foreach (Kund kund in kunderTillForetaget) 
+            {
+                if (kund.Name == "Alligator")
+                {
+                    resultOfWhere.Add(kund);
+                }
+            }
+
+            List<string> descr = new List<string>();
+
+            foreach (Kund kund in resultOfWhere)
+            { 
+                descr.Add(kund.Description);
+            }
+
+            List<string> filterDescr = new List<string>();
+
+            foreach(string description in descr)
+            {
+                if (description == "Sköldpadda")
+                {
+                    filterDescr.Add(description);
+                }
+            }
+
+            if (Something[0].Equals(filterDescr[0]))
+            {
+                Console.WriteLine("Success!");
+            }
+
+        }
+    }
+}
